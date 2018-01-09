c3b4b4f68f939e6ad8f23f7c52d4b1e122ff0063
Serhii Londar <serhii.londar@gmail.com>
2018-01-10 00:14:55 +0200

    Update .travis.yml Remove Pods folder from example project

--- Example/Pods/Target Support Files/GithubAPI/GithubAPI-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_GithubAPI : NSObject
-@end
-@implementation PodsDummy_GithubAPI
-@end
--- Example/Pods/Target Support Files/Pods-GithubAPI_Tests/Pods-GithubAPI_Tests-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_Pods_GithubAPI_Tests : NSObject
-@end
-@implementation PodsDummy_Pods_GithubAPI_Tests
-@end

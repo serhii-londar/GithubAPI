d4f6e9ff2c2d7c11f6fb62b3fef33c7dcd835eb3
Serhii Londar <serhii.londar@gmail.com>
2020-03-06 17:04:15 +0200

    Fix swift package manager support

--- Package.swift
+++ Package.swift
@@ -1,9 +1,12 @@
-// swift-tools-version:4.0
+// swift-tools-version:5.0
 // The swift-tools-version declares the minimum version of Swift required to build this package.
 import PackageDescription
 
 let package = Package(
     name: "GithubAPI",
+    platforms: [
+        .iOS(.v9)
+    ],
     products: [
         .library(name: "GithubAPI", targets: ["GithubAPI"])
         ],
